bcdb4748b36f4fb7b59f000f0a12d7767d085dad
Johan Andersson <johan.peter.andersson@gmail.com>
2017-04-10 20:59:53 +0200

    day3 part two

--- day3/src/day3/core.clj
+++ day3/src/day3/core.clj
@@ -21,16 +21,45 @@
   [line]
   (map #(Integer/parseInt %) (str/split (str/trim line) #"\s+")))
 
-(defn parse-input
-  [raw-input]
-  (map parse-line (str/split raw-input #"\n")))
+(defn add-edge
+  [ll col edge]
+  (update ll col #(conj % edge)))
 
-(defn get-input
+(defn add-lines
+  [ll lines]
+  (if (empty? lines)
+      ll
+      (let [line (first lines)
+             a (add-edge ll :col0 (nth line 0))
+             b (add-edge a  :col1 (nth line 1))
+             c (add-edge b  :col2 (nth line 2))]
+             (add-lines c (rest lines)))))
+
+(def empty-ll
+  {:col0 [] :col1 [] :col2 []})
+
+(defn get-input-lines
   []
-  (parse-input (read-input-file)))
+  (map parse-line (str/split (read-input-file) #"\n")))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (count-triangles (get-input)))
+(defn get-input-2
+  []
+    (def three-cols (add-lines empty-ll (get-input-lines)))
+    (def a (:col0 three-cols))
+    (def b (:col1 three-cols))
+    (def c (:col2 three-cols))
+    (partition 3 (concat a b c)))
+
+(defn part-two
+  []
+  (count-triangles (get-input-2)))
+
+
+(defn get-input-1
+  []
+  (get-input-lines))
+
+(defn part-one
+  []
+  (count-triangles (get-input-1)))
 
