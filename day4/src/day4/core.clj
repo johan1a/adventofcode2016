780db740f0676e3a8a01823a9ad8f8d2d5e2c818
Johan Andersson <johan.peter.andersson@gmail.com>
2017-04-11 21:15:03 +0200

    day 4 part two

--- day4/src/day4/core.clj
+++ day4/src/day4/core.clj
@@ -58,7 +58,79 @@
     (let [s (sum-if-valid (first lines))]
      (+ s (sum-ids (rest lines))))))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(def alphabet (seq "abcdefghijklmnopqrstuvwxyz"))
+
+(def alphabet-size (count alphabet))
+
+(defn alpha-index
+  [ch]
+  (.indexOf alphabet ch))
+
+(defn total-offset
+  [ch n]
+  (mod (+ (alpha-index ch) n) alphabet-size))
+
+(defn shift-char
+  [ch n]
+  (if (= ch \-)
+    \space
+    (nth alphabet (total-offset ch n))))
+
+(defn shift-string
+  [s n]
+  (str/join (map #(shift-char % n) s)))
+
+(defn decode
+  [x]
+  (shift-string (:letters x) (:sector-id x)))
+
+(defn make-decoded
+  [line]
+  (let [x (parse-line line)]
+  {(decode x) (:sector-id x)}))
+
+(defn decode-lines
+  [lines]
+  (if (empty? lines)
+    {}
+    (merge (make-decoded (first lines)) (decode-lines (rest lines)))))
+
+(defn find-north-pole-objects
+  []
+  (filter #(.contains (first %) "north") (decode-lines (get-lines))))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
