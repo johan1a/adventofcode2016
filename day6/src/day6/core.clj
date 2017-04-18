24988ff434fe5cdeb8996dc1076f5e15be4826c8
Johan Andersson <johan.peter.andersson@gmail.com>
2017-04-18 13:36:43 +0200

    day6 part1

---
+++ day6/src/day6/core.clj
@@ -0,0 +1,44 @@
+(ns day6.core
+  (:gen-class))
+(require '[clojure.string :as str])
+
+(defn read-file
+  [file-name]
+  (map str/trim (str/split (slurp file-name) #"\n")))
+
+(defn test-input
+  []
+  (read-file "testinput.txt"))
+
+(defn parse-line
+  [line]
+  (map-indexed (fn [i a] [i a]) line))
+
+(defn columns
+  [msgs]
+  (partition (count msgs) (apply interleave msgs)))
+
+(defn make-input
+  [lines]
+  (map parse-line lines))
+
+(defn get-frequencies
+  [msgs]
+  (map #(sort-by second %) (map frequencies (columns msgs))))
+
+(defn error-correct
+  [msgs]
+  (str/join (map first (map last (get-frequencies msgs)))))
+
+(defn test-part1
+  []
+  (error-correct (test-input)))
+
+(defn part1
+  []
+  (error-correct (read-file "input.txt")))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
