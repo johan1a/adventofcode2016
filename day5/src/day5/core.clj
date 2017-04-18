34d4e9f350300a326c67f59ad895bc9dfddd2dad
Johan Andersson <johan.peter.andersson@gmail.com>
2017-04-18 12:24:58 +0200

    day 5 part 2

--- day5/src/day5/core.clj
+++ day5/src/day5/core.clj
@@ -29,6 +29,43 @@
       (str (:letter l) 
             (get-password s (dec n) (inc (:index l)))))))
 
+(defn good-hash? [h]
+  (and (str/starts-with? h "00000") 
+       (Character/isDigit (nth h 5))))
+
+(defn make-obj [h i]
+  {:index i :pos (Integer/parseInt (str (nth h 5))) :letter (nth h 6) :hash h})
+
+(defn free-pos [f pos]
+  (not (contains? (set (map :pos f)) pos)))
+
+(defn next-letter2 [s i f]
+  (let [h (get-hash s i)]
+   (if (and (good-hash? h))
+     (let [x (make-obj h i)
+           pos (:pos x)]
+     (if (and (>= pos 0) (< pos 8) (free-pos f pos))
+         x
+         (recur s (inc i) f)))
+     (recur s (inc i) f))))
+
+(defn get-password-unordered-map [s n i f]
+    (if (= 0 n)
+      []
+      (let [l (next-letter2 s i f)
+            prev-found (conj f l)]
+        (println (str "found hash: " l))
+        (conj (get-password-unordered-map s (dec n) (inc (:index l)) prev-found) l))))
+
+(defn get-password-unordered [s n i]
+  (let [p (get-password-unordered-map s n i [])
+       x (map (fn [a] [(:pos a) (:letter a)]) p)]
+   (str/join (map second (sort x)))))
+
+(defn door-password2
+  []
+  (get-password-unordered door-id 8 0))
+
 (defn door-passord
   []
   (get-password door-id 8 0))
