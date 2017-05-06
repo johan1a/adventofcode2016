6b3c3975fd1392d4006b64f86c9c8bb93307b019
Johan Andersson <johan.peter.andersson@gmail.com>
2017-05-06 11:43:26 +0200

    possible-moves works with new representation

--- day11/src/day11/core.clj
+++ day11/src/day11/core.clj
@@ -6,11 +6,6 @@
 (require '[clojure.math.combinatorics :as combo])
 (use '[clojure.pprint :only (pprint)])
 
-(defn floor-ok?
-  [device]
-  (let [floor (:floor device)]
-    (and (>= floor 0) (<= floor 4))))
-
 (def all-floors [0 1 2 3])
 (def not-found -1)
 (def goal-floor 3)
@@ -69,15 +64,6 @@
   [devices]
   (every? true? (map #(chips-ok? devices %) all-floors)))
 
-(defn valid-state?
-  [state]
-  (if (or 
-        (some #(not (floor-ok? %)) (:devices state))
-        (or (< (:elevator state) 0 )
-            (> (:elevator state) max-floor)))
-    false
-    (devices-ok? (:devices state))))
-
 (defn flatten-one
   "Flatten one level"
   [ll]
@@ -105,20 +91,138 @@
         up-state {:devices up-devices :elevator (inc elevator)}]
      (filter #(valid-state? %) [down-state up-state])))
 
-(defn possible-moves-floor 
+
+; en för varje unik med två 0, och en för varje unik kombintion med en i varje upp,
+; och (evenutellt går att optimera bort) en för varje ed en 0 med endast en uppdaterad
+
+; map inc2 pairs + 
+
+
+; behöver typ replaceone
+;incenivarje cc = 1010 1001 0110 0101
+; om båda är = 0, returnera ett state där dessa två par har dessa positioner ökade
+; mxa fyra nya statejjhh
+
+; cc = (combo/combinations pairs 2)
+;(map (incenivarje pairs) cc)
+
+; + samma som ovan fast för bara ett par i taget, där man ökar en 
+
+; +
+;  map 
+
+; variations to increase / decrease one pair
+(def offsets-1    [[0 1]
+                   [1 0]
+                   [1 1]])
+
+;variations to increase / decrease a combinations of two pars.
+(def offsets-2      [
+;                   [0 0 0 0]
+                   [0 0 0 1] 
+                   [0 0 1 0] 
+                   [0 0 1 1]
+                   [0 1 0 0]
+                   [0 1 0 1]
+                   [0 1 1 0]
+;                   [0 1 1 1]
+                   [1 0 0 0]
+                   [1 0 0 1]
+                   [1 0 1 0]
+                   [1 1 0 0]]
+;                   [1 1 0 1]
+;                   [1 1 1 0]
+;                   [1 1 1 1]])
+)
+
+(defn mul
+  [v1 v2]
+  [(* (first v1) (first v2)) (* (second v1) (second v2))])
+
+(defn matches-offset
+  [pair offset floor]
+  (= (mul offset [floor floor]) (mul pair offset)))
+
+(defn add-offset-1
+  [pair offset sign]
+   (map + pair (mul offset [sign sign])))
+
+(defn make-offset-state-1
+  [pairs pair offset floor sign]
+  (if (matches-offset pair offset floor)
+      (let [
+            new-pair (add-offset-1 pair offset sign)
+            index (.indexOf pairs pair)
+            new-pairs (assoc pairs index new-pair)
+            ]
+        {:pairs  new-pairs :elevator (+ sign floor)})
+      false))
+
+(defn make-offset-state-2
+  [pairs pair-comb offset floor sign]
+  (let [offset1 (take 2 offset)
+        offset2 (drop 2 offset)
+        state (make-offset-state-1 pairs (first pair-comb) offset1 floor sign)]
+        (if state
+            (let [pairs2 (:pairs (make-offset-state-1 pairs (first pair-comb) offset1 floor sign))
+                  res (make-offset-state-1 pairs2 (second pair-comb) offset2 floor sign)]
+            res)
+            false)))
+
+; move parts in one pair
+(defn move-one-pair
+  [pairs pair floor sign]
+  (filter #(not (false? %)) (map #(make-offset-state-1 pairs pair % floor sign) offsets-1)))
+
+; move parts in two different pairs
+(defn move-two-pairs
+  [pairs pair-comb floor sign]
+  (filter #(not (false? %)) (map #(make-offset-state-2 pairs pair-comb % floor sign) offsets-2)))
+
+
+(defn pair-ok?
+  [pairs pair]
+  (if (= (first pair) (second pair))
+      true
+      (not-any? #(= (second pair) (first %)) (disj (set pairs) pair))))
+
+(defn pairs-ok?
+  [pairs]
+  (every? #(pair-ok? pairs %) pairs))
+
+(defn floors-ok?
+  [pair]
+  (and (not-any? #(< % 0) pair)
+       (not-any? #(> % max-floor) pair)))
+
+(defn elevator-ok?
   [state]
-  (let [elevator (:elevator state)
-        devices (:devices state)
-        at-floor (filter #(= elevator (:floor %)) devices)
-        comb-1 (combinations at-floor 1)
-        comb-2 (combinations at-floor 2)
-        combs (apply conj comb-1 comb-2)]
-    (flatten-one (map #(up-and-down devices % elevator) combs))))
+  (or (>= (:elevator state) 0 )
+      (<= (:elevator state) max-floor)))
+
+(defn valid-state?
+  [state]
+  (if (or 
+        (some #(not (floors-ok? %)) (:pairs state))
+        (not (elevator-ok? state)))
+    false
+    (pairs-ok? (:pairs state))))
 
 (defn possible-moves
   [state]
-  (let [ moves (possible-moves-floor state)]
-    moves))
+  (let [pairs (:pairs state)
+        floor (:elevator state)
+        comb-1 (distinct pairs)
+        comb-2 (combinations pairs 2)
+        up-states-1 (map #(move-one-pair pairs % floor 1) comb-1)
+        down-states-1 (map #(move-one-pair pairs % floor -1) comb-1)
+        up-states-2 (map #(move-two-pairs pairs % floor 1) comb-2)
+        down-states-2 (map #(move-two-pairs pairs % floor -1) comb-2)
+        ]
+    (filter valid-state?
+            (distinct 
+              (flatten 
+                (concat up-states-2 up-states-1 down-states-1 down-states-2 ))))))
 
 (defn goal-state?
   [state]
@@ -128,69 +232,12 @@
         ]
     res))
 
-(def visited (ref #{}))
-
-(defn get-visited []
-  @visited)
-
-(defn update-visited [v]
-  (dosync (ref-set visited v)))
-
-(def results (ref #{}))
-
-(defn get-results [] @results)
-
-(defn update-results [v]
-  (dosync (ref-set results v)))
-
-(defn make-result
-  [steps path]
-  {:steps steps :path path})
-
-(defn get-best
-  [results]
-  (let [sorted (sort-by :steps (filter #(not (= not-found (:steps %))) results))]
-    (if (= 0 (count sorted)) 
-      {:steps not-found}
-      (first sorted))))
-
-(defn search-recursive
-  [state nbr-moves path]
-;  (pprint (str "nbr visited: " (count (get-visited))))
-;  (if (contains? tt state) (pprint state))
- ; (print "Current state: ")
-; (pprint state)
- ; (if (= t9 state) (println "at t9"))
-  (if (goal-state? state) (make-result nbr-moves (conj path state)) 
-     (let [new-visited (conj (get-visited) state)
-           x (update-visited new-visited)
-           new-path (conj path state)
-           all-possible (possible-moves state)
-           moves (filter #(not (contains? new-visited %)) all-possible)
-            
-          ;ff  (if (= t9 state) (println "all-possible has t10?"))
-          ;ff  (if (= t9 state) (println (contains? (set all-possible) t10)))
-          ; xx (if (contains? tt state) (print "Possible moves: " ))
-          ; xxx (if (contains? tt state) (pprint all-possible))
-        ;   ll (print \newline)
-         ;  l1 (pprint "Possible moves: " )
-          ; l2 (pprint all-possible)
-           ;x9 (println (read-line))
-
-           results (map #(search-recursive % (inc nbr-moves) new-path) moves)
-           ]
-       (if (= 0 (count moves))
-         {:steps not-found}
-          (get-best results))
-     )))
-
 (defn neighbors
   [state S]
       (do (let [possible (possible-moves state)
          ;   p (println "in neighbors")
             ]
        (filter #(not (contains? (set S) %)) possible))))
-
 
 (defn follow 
   ([n parents1] (follow n parents1 []))
@@ -200,19 +247,21 @@
               (recur parent parents1 (cons parent path))
               path))))
 
-(defn make-goal
-  [current goals]
-  (conj goals {:goal current 
-               :parents (get-Parents) 
-               :length (count (follow current (get-Parents)))}))
-
 (defn assoc-multiple
   [map1 keys1 value]
   (reduce #(assoc %1 %2 value) map1 keys1))
 
+(defn state-cost-factor
+  [x]
+  (- max-floor (:elevator x)))
+
+(defn cost
+  [q x]
+  (* (state-cost-factor x) (get q x)))
+
 (defn get-min
   [q]
-  (first (sort-by (fn [x] (get q x)) (keys q))))
+  (first (sort-by #(cost q %) (keys q))))
 
 (defn get-dist
   [dists k]
@@ -225,7 +274,7 @@
            dist (assoc {} src 0)
            prev {}]
       (if (= (count q) 0)
-        [dist prev]
+        {:dists dist :prevs prev}
         (let [u  (get-min q)
               ll (loop [nn (neighbors u {})
                         q2 (dissoc q u)
@@ -237,10 +286,11 @@
                                 alt (+ 1 (get dist2 u))
                                 dist-v (get-dist dist2 v)]
                             (if (< alt dist-v) 
-                                    (recur (rest nn)
+                                    (do (println alt)
+                                      (recur (rest nn)
                                        (assoc q2 v alt)
                                        (assoc dist2 v alt)
-                                       (assoc prev2 v u))
+                                       (assoc prev2 v u)))
                                     (recur (rest nn)
                                        q2
                                        dist2
@@ -248,160 +298,33 @@
                         (recur (dissoc (get ll 0) u) ; remove u from q
                                (get ll 1)
                                (get ll 2))))))
-  
-(defn do-search
-  [state]
-  (update-visited #{})
-  (let [results (search-recursive state 0 [])]
-    results))
 
-(defn best-solution
-  [solutions]
-  (apply min (filter #(> % 0) solutions)))
-
-(def small-state
-  { :devices
-    #{{:type "generator" :substance "promethium" :floor 0} 
-     {:type "generator" :substance "einsteinium" :floor 2}
-     {:type "generator" :substance "hydrogen" :floor 2}}
-   :elevator 0})
-
-(def s1{:devices
-  #{{:type "generator", :substance "hydrogen", :floor 3}
-    {:type "generator", :substance "promethium", :floor 2}},
-  :elevator 2})
-
-
-
-(def test-state
-  { :devices
-    #{{:type "microchip" :substance "hydrogen" :floor 0} 
-     {:type "microchip" :substance "lithium" :floor 0}
-     {:type "generator" :substance "hydrogen" :floor 1} 
-     {:type "generator" :substance "lithium" :floor 2}}
-    :elevator 0})
-
-(def t1
-  { :devices
-    #{{:type "microchip" :substance "hydrogen" :floor 1} 
-     {:type "microchip" :substance "lithium" :floor 0}
-     {:type "generator" :substance "hydrogen" :floor 1} 
-     {:type "generator" :substance "lithium" :floor 2}}
-    :elevator 1})
-
-(def t2
-  { :devices
-    #{{:type "microchip" :substance "hydrogen" :floor 2} 
-     {:type "microchip" :substance "lithium" :floor 0}
-     {:type "generator" :substance "hydrogen" :floor 2} 
-     {:type "generator" :substance "lithium" :floor 2}}
-    :elevator 2})
-
-(def t3
-  { :devices
-    #{{:type "microchip" :substance "hydrogen" :floor 1} 
-     {:type "microchip" :substance "lithium" :floor 0}
-     {:type "generator" :substance "hydrogen" :floor 2} 
-     {:type "generator" :substance "lithium" :floor 2}}
-    :elevator 1})
-
-(def t4
-  { :devices
-    #{{:type "microchip" :substance "hydrogen" :floor 0} 
-     {:type "microchip" :substance "lithium" :floor 0}
-     {:type "generator" :substance "hydrogen" :floor 2} 
-     {:type "generator" :substance "lithium" :floor 2}}
-    :elevator 0})
-
-(def t5
-  { :devices
-    #{{:type "microchip" :substance "hydrogen" :floor 1} 
-     {:type "microchip" :substance "lithium" :floor 1}
-     {:type "generator" :substance "hydrogen" :floor 2} 
-     {:type "generator" :substance "lithium" :floor 2}}
-    :elevator 1})
-
-(def t6
-  { :devices
-    #{{:type "microchip" :substance "hydrogen" :floor 2} 
-     {:type "microchip" :substance "lithium" :floor 2}
-     {:type "generator" :substance "hydrogen" :floor 2} 
-     {:type "generator" :substance "lithium" :floor 2}}
-    :elevator 2})
-
-(def t7
-  { :devices
-    #{{:type "microchip" :substance "hydrogen" :floor 3} 
-     {:type "microchip" :substance "lithium" :floor 3}
-     {:type "generator" :substance "hydrogen" :floor 2} 
-     {:type "generator" :substance "lithium" :floor 2}}
-    :elevator 3})
-
-(def t8
-  { :devices
-    #{{:type "microchip" :substance "hydrogen" :floor 2} 
-     {:type "microchip" :substance "lithium" :floor 3}
-     {:type "generator" :substance "hydrogen" :floor 2} 
-     {:type "generator" :substance "lithium" :floor 2}}
-    :elevator 2})
-
-(def t9
-  { :devices
-    #{{:type "microchip" :substance "hydrogen" :floor 2} 
-     {:type "microchip" :substance "lithium" :floor 3}
-     {:type "generator" :substance "hydrogen" :floor 3} 
-     {:type "generator" :substance "lithium" :floor 3}}
-    :elevator 3})
-
-(def t10
-  { :devices
-    #{{:type "microchip" :substance "hydrogen" :floor 2} 
-     {:type "microchip" :substance "lithium" :floor 2}
-     {:type "generator" :substance "hydrogen" :floor 3} 
-     {:type "generator" :substance "lithium" :floor 3}}
-    :elevator 2})
-
-(def t11
-  { :devices
-    #{{:type "microchip" :substance "hydrogen" :floor 3} 
-     {:type "microchip" :substance "lithium" :floor 3}
-     {:type "generator" :substance "hydrogen" :floor 3} 
-     {:type "generator" :substance "lithium" :floor 3}}
-    :elevator 3})
-
-(def t0 test-state)
+(defn follow-path
+  ([prevs curr path]
+  (let [prev (get prevs curr)]
+    (if prev
+      (recur prevs prev (cons curr path))
+      path
+      ))))
 
 (def tt #{t0 t1 t2 t3 t4 t5 t6 t7 t8 t9 t10 t11})
 
-(def initial-state
-  {:devices
-    #{{:type "generator" :substance "promethium" :floor 0} 
-     {:type "microchip" :substance "promethium" :floor 0}
-     {:type "generator" :substance "cobalt" :floor 1} 
-     {:type "generator" :substance "curium" :floor 1}
-     {:type "generator" :substance "ruthenium" :floor 1}
-     {:type "generator" :substance "plutonium" :floor 1}
-     {:type "microchip" :substance "cobalt" :floor 2}
-     {:type "microchip" :substance "curium" :floor 2}
-     {:type "microchip" :substance "ruthenium" :floor 2}
-     {:type "microchip" :substance "plutonium" :floor 2}}
+; pairs: [generator, microchip]
+(def x0
+  {:pairs
+    [ [ 0  0 ]
+      [ 1  2 ]
+      [ 1  2 ]
+      [ 1  2 ]
+      [ 1  2 ] ]
    :elevator 0})
 
-(def part1-goal
-  {:devices
-    #{{:type "generator" :substance "promethium" :floor 3} 
-     {:type "microchip" :substance "promethium" :floor 3}
-     {:type "generator" :substance "cobalt" :floor 3} 
-     {:type "generator" :substance "curium" :floor 3}
-     {:type "generator" :substance "ruthenium" :floor 3}
-     {:type "generator" :substance "plutonium" :floor 3}
-     {:type "microchip" :substance "cobalt" :floor 3}
-     {:type "microchip" :substance "curium" :floor 3}
-     {:type "microchip" :substance "ruthenium" :floor 3}
-     {:type "microchip" :substance "plutonium" :floor 3}}
-   :elevator 3})
-
-
-
-
+(def xt
+  {:pairs
+    [ [ 0  0 ]
+      [ 0  2 ]
+      [ 1  0 ]
+      [ 0  2 ]
+      [ 1  2 ] ]
+   :elevator 0})
 
