3a00cdaf50acfa75d65a6399f8d8a2c85e4c10f9
Johan Andersson <joha@netcompany.com>
2017-05-25 19:53:27 +0200

    day 20 part two

--- day20/src/day20/core.clj
+++ day20/src/day20/core.clj
@@ -24,22 +24,30 @@
   (find-lowest (get-input)))
 
 
-(def max-val 4294967295)
+(def real-max-val 4294967295)
 
 (defn count-free
-  ([ranges] (count-free ranges 0))
-  ([ranges sum]
-   (if (= 1 (count ranges)) sum
-     (let [a (first ranges)
-           b (second ranges)
-           gap (- (first b) (second a))]
-       (recur (rest ranges) (+ sum gap))))))
+  ([max-val ranges] (count-free max-val ranges 0))
+  ([max-val ranges sum]
+   (if (= 1 (count ranges)) (+ sum (- max-val (second (first ranges))))
+     (let [first-high (second (first ranges))
+           second-low (first (second ranges))
+           upper-limit (min second-low max-val)
+           gap (- (- upper-limit first-high) 1)
+           new-sum (+ sum gap)]
+       (if (> second-low max-val) new-sum
+       (recur max-val (rest ranges) new-sum))))))
 
 (defn find-ranges
   ([input] (find-ranges [[-1 -1]] input))
   ([ranges input]
-  (if (empty? input) (count-free (reverse ranges))
+  (if (empty? input) ranges
     (let [[low high] (first ranges)
           [input-low input-high] (first input)]
       (if (> input-low (+ 1 high)) (recur (cons [input-low input-high] ranges) (rest input))
         (recur (cons [low (max high input-high)] (rest ranges)) (rest input)))))))
+
+(defn part-two
+  []
+ (count-free real-max-val (reverse (find-ranges (get-input)))))
+
