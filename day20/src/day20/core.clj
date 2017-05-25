8c0decea209fe71e12fcf0a8d93e079cc5409f2b
Johan Andersson <joha@netcompany.com>
2017-05-25 17:32:59 +0000

    day 20 part two approach

--- day20/src/day20/core.clj
+++ day20/src/day20/core.clj
@@ -23,3 +23,23 @@
   []
   (find-lowest (get-input)))
 
+
+(def max-val 4294967295)
+
+(defn count-free
+  ([ranges] (count-free ranges 0))
+  ([ranges sum]
+   (if (= 1 (count ranges)) sum
+     (let [a (first ranges)
+           b (second ranges)
+           gap (- (first b) (second a))]
+       (recur (rest ranges) (+ sum gap))))))
+
+(defn find-ranges
+  ([input] (find-ranges [[-1 -1]] input))
+  ([ranges input]
+  (if (empty? input) (count-free (reverse ranges))
+    (let [[low high] (first ranges)
+          [input-low input-high] (first input)]
+      (if (> input-low (+ 1 high)) (recur (cons [input-low input-high] ranges) (rest input))
+        (recur (cons [low (max high input-high)] (rest ranges)) (rest input)))))))
