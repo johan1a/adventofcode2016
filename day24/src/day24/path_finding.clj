47e216af1867eed196c8db3a6e5e9a2a1a7f860c
Johan Andersson <joha@netcompany.com>
2017-10-27 15:02:44 +0200

    day24 backup

---
+++ day24/src/day24/path_finding.clj
@@ -0,0 +1,146 @@
+(ns day24.path-finding
+  (:gen-class)
+  (:require [clojure.string :as str]
+            [clojure.data.priority-map :refer :all]))
+(use '[clojure.pprint :only (pprint)])
+
+(defn abs
+  [x]
+  (if (> x 0) x (* x -1)))
+
+(def start-state [0 0])
+
+(defn neigh-matrix
+  [-1 -1
+   -1  0
+   -1  1
+   0  -1
+   0   1
+   1  -1
+   1   0
+   1   1])
+
+(defn add-states
+  [a b]
+  (map + a b))
+
+(defn in-matrix?
+  [neigh-matrix x]
+  (let [i (first x)
+        j (second x)
+        maxi (count neigh-matrix)
+        maxj (count (first neigh-matrix))]
+    (and
+      (i >= 0)
+      (j >= 0)
+      (i < maxi)
+      (j - maxj))))
+
+(defn neighbors
+  ([state neigh-matrix]
+   (let [neigh-poses (filter in-matrix? neigh-matrix (map add-states state neigh-poses))
+
+         ]
+   "todo"))
+
+(defn get-dist
+  [dists k default]
+   (let [dist (get dists k)]
+     (if dist dist default)))
+
+(defn should-terminate?
+  [open dists goal]
+      (= 0 (count open)))
+
+(defn find-by-pos
+  [m v]
+  (first (first (filter #(= (:pos (first %)) (:pos v)) m))))
+
+(defn check-neighbors
+  [heuristic open closed nn fscores dists prevs curr goal comp-func default-dist]
+    (if (= 0 (count nn))
+      [open closed fscores dists prevs]
+      (let [v (first nn)
+            tentative (+ 1 (get dists curr))
+            dist-v (get-dist dists v default-dist)]
+        (if (comp tentative dist-v)
+            (recur heuristic
+                   (assoc open v (+ tentative (heuristic v goal)))
+                   closed
+                   (rest nn)
+                   (assoc fscores v (+ tentative (heuristic v goal)))
+                   (assoc dists v tentative)
+                   (assoc prevs v curr)
+                   curr
+                   goal
+                   comp-func
+                   default-dist)
+          (recur heuristic
+                 open closed
+                 (rest nn)
+                 fscores
+                 dists
+                 prevs
+                 curr
+                 goal
+                 comp-func
+                 default-dist)))))
+
+(defn shortest-path
+  [heuristic src goal neigh-matrix]
+   (loop [open (priority-map src (heuristic src goal))
+          closed #{}
+          fscores (priority-map src (heuristic src goal))
+          dists (priority-map src 0 goal Integer/MAX_VALUE)
+          prevs {}]
+     (if (should-terminate? open dists goal)
+       dists
+       (let [curr (first (peek open))
+             res (check-neighbors heuristic
+                                  (pop open)
+                                  (conj closed curr)
+                                  (neighbors curr neigh-matrix)
+                                  fscores
+                                  dists
+                                  prevs
+                                  curr
+                                  goal
+                                  (fn [a b] (< a b))
+                                  Integer/MAX_VALUE) ]
+         (recur (get res 0)
+                (get res 1)
+                (dissoc (get res 2) curr) ; remove curr from fscores
+                (get res 3)
+                (get res 4))))))
+
+(defn parse-line
+  [line]
+  (str/split line #""))
+
+(defn get-lines
+  [txt]
+  (str/split txt #"\n"))
+
+(defn read-input
+  [file-name]
+  (map parse-line (get-lines (slurp file-name))))
+
+(defn heuristic1
+  [a b]
+  (reduce + (map abs ( b a))))
+
+(defn solve
+  []
+  (subs (:path (shortest-path heuristic1 start-state nil)) (count (:path start-state))))
+
+(def start "TODO")
+
+(defn part-one
+  []
+  (solve start))
+
+(defn find-distances
+  [file-name]
+  (read-input file-name))
+
+(read-input "input.txt")
